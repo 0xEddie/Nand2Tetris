e0b8a5460e752c1977ffd44c6c5695618f8291b4
ede <eomeasoo@ualberta.ca>
2022-07-20 14:39:44 -0600

    implement chained hash table

--- projects/06/hashTable.js
+++ projects/06/hashTable.js
@@ -22,9 +22,40 @@ class HashTable {
 		const idx = hashStringToInt(key, this.table.length);
 		return this.table[idx];
 	}
-
 }
 
-const myTable = new HashTable();
+class ChainedHashTable {
+
+	table = new Array(19);
+	
+	setItem = (key, value) => {
+		const idx = hashStringToInt(key, this.table.length);
+		if (this.table[idx]) {
+			this.table[idx].push([key, value]);
+		} else {
+			this.table[idx] = [[key, value]];
+		}
+	}
+
+	getItem = (key) => {
+		const idx = hashStringToInt(key, this.table.length);
+		// if this key doesn't exist yet return null
+		if (!this.table[idx]) {
+			return null;
+		}
+		// find the of the first array that matches the key
+		// return the second elem of array (ie. the value, of the KVP)
+		return this.table[idx].find(ind => ind[0] === key)[1];
+	};
+}
+
+const myTable = new ChainedHashTable();
 myTable.setItem('firstName', 'bob');
+myTable.setItem('lastName', 'tim');
+myTable.setItem('age', 5);
+myTable.setItem('dob', '1/2/3');
+
 console.log(myTable.getItem('firstName'));
+console.log(myTable.getItem('lastName'));
+console.log(myTable.getItem('age'));
+console.log(myTable.getItem('dob'));
