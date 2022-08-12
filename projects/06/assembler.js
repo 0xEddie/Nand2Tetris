0383116f736b2ee8e703d08372e25205fcff4e01
= <=>
2022-08-11 20:06:07 -0600

    Predefined symbols in SymbolTable

--- projects/06/assembler.js
+++ projects/06/assembler.js
@@ -173,8 +173,35 @@ class Code {
 class SymbolTable {
   constructor() {
     // No return value
-    // Creates new empty symbol table
-    this.symbols = {};
+    // Creates new empty symbol table with all predefined symbols and pre-allocated RAM adr
+    this.symbols = {
+      'SP': 0,
+      'LCL': 1,
+      'ARG': 2,
+      'THIS': 3,
+      'THAT': 4,
+      'R0': 0,
+      'R1': 1,
+      'R2': 2,
+      'R3': 3,
+      'R4': 4,
+      'R5': 5,
+      'R6': 6,
+      'R7': 7,
+      'R8': 8,
+      'R9': 9,
+      'R10': 10,
+      'R11': 11,
+      'R12': 12,
+      'R13': 13,
+      'R14': 14,
+      'R15': 15,
+      'SCREEN': 16384,
+      'KBD': 24576
+    };
+    
+    this.romAddresses = 0;
+    
   }
   
   addEntry (symbol, address) {
@@ -205,6 +232,13 @@ function main() {
   const parser = new Parser(filePath);
   let line, cmdType;
   const code = new Code();
+  
+  // build symbol table
+  const st = new SymbolTable();
+  // 
+  while (parser.hasMoreCommands()) {
+    
+  }
   
   while (parser.hasMoreCommands()) {
     // move to next command in file
